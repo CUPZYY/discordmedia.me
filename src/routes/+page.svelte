34cc06463f10db7cac521c01c61ff4c88d7c0026
CUPZYY <richbanana1234@gmail.com>
2024-06-02 21:19:18 +0200

    remove form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,22 @@
 <script>
-    let file;
+    let files;
+    $: console.log(files?.[0].name);
 </script>
 
 <section class="section is-medium">
     <h1 class="title is-1 has-text-centered">discord video</h1>
     <div class="container block">
         <div class="container has-text-centered">
-            <form class="block" id="upload-form" enctype="multipart/form-data" method="post" action="https://up1.fileditch.com/upload.php">
-                <div class="file is-boxed has-name is-primary is-centered">
-                    <label class="file-label">
-                        <input class="file-input" type="file" name="files[]" bind:files={file}>
-                        <span class="file-cta">
-                            <span class="file-label"> Upload! </span>
-                        </span>
-                        <span class="file-name"> {file?.[0].name ?? "No file uploaded"} </span>
-                    </label>
-                </div>
-            </form>
+            <div class="file is-boxed has-name is-primary is-centered">
+                <label class="file-label">
+                    <input class="file-input" type="file" name="files[]" bind:files={files}>
+                    <span class="file-cta">
+                        <span class="file-label"> Upload! </span>
+                    </span>
+                    <span class="file-name"> {files?.[0].name ?? "No file uploaded"} </span>
+                </label>
+            </div>
             <input type="text" class="input is-success" readonly>
-
         </div>
     </div>
 </section>
