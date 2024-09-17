5318098ab120d92d57fd27c5193bdf2add7f5fb3
CUPZYY <richbanana1234@gmail.com>
2024-09-17 11:14:55 +0200

    using .env path for db

--- src/lib/datebase/client.ts
+++ src/lib/datebase/client.ts
@@ -1,11 +1,12 @@
 import { drizzle, type BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import { type InsertUrl, type Url, urlTable } from "./schema";
 import Database from "better-sqlite3";
+import { DB_PATH } from "$env/static/private";
 
-const sqlite = new Database("database.db");
+const sqlite = new Database(DB_PATH);
 const db: BetterSQLite3Database = drizzle(sqlite)
 
-const result: Url[] = db.select().from(urlTable).all()
+export const result: Url[] = db.select().from(urlTable).all()
 
 export const insertUrl = (url: InsertUrl) => {
     return db.insert(urlTable).values(url).run()
