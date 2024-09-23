41026fcd85acbdd379988c122f256069907fb6ce
CUPZYY <richbanana1234@gmail.com>
2024-09-23 22:58:53 +0200

    Added a length-parameter to generateID

--- src/lib/datebase/wrapper.ts
+++ src/lib/datebase/wrapper.ts
@@ -16,6 +16,6 @@ export async function getData(id: string) {
     return (await db.select().from(urlTable).where(eq(urlTable.id, id)))[0];
 }
 
-export async function generateID() {
+export async function generateID(length: number = 6) {
     return (Math.random() + 1).toString(36).slice(-length);
 }
