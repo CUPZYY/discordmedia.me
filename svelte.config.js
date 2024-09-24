75a007037973957a6b10bb611317957f8c88c7ee
CUPZYY <richbanana1234@gmail.com>
2024-09-24 20:50:30 +0200

    change adapter to node

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-node";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		scss: {
-			prependData: "@import './src/app.scss';",
-		}
-	}),
+    preprocess: preprocess({
+        scss: {
+            prependData: "@import './src/app.scss';",
+        },
+    }),
     // Remove vite-plugin-svelte warnings about unused CSS selectors
     onwarn: (warning, handler) => {
         const { code, frame } = warning;
-        if (code === 'css-unused-selector') return;
+        if (code === "css-unused-selector") return;
         handler(warning);
     },
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter(),
+    },
 };
 
 export default config;
