bd95e2c2ae7cea755c833ea82ddc75254e3d4ebd
CUPZYY <richbanana1234@gmail.com>
2024-09-30 21:02:01 +0200

    remove unused div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,37 +43,35 @@
             <h1 class="title is-1 has-text-centered">discord video</h1>
         </a>
     </div>
-    <div class="container block">
-        <div class="container has-text-centered">
-            <div
-                class="file is-boxed has-name {files?.[0]
-                    ? 'is-success'
-                    : 'is-primary'} is-centered"
-            >
-                <label class="file-label">
-                    <input
-                        class="file-input"
-                        type="file"
-                        accept="video/mp4, video/quicktime"
-                        bind:files
-                    />
-                    <span class="file-cta">
-                        <span class="file-label"> Upload! </span>
-                    </span>
-                    <span class="file-name">
-                        {files?.[0].name ?? "No file uploaded"}
-                    </span>
-                </label>
-            </div>
-            <progress class="progress is-primary" value={progress} max="100"
-            ></progress>
-            <input
-                type="text"
-                placeholder="No file uploaded"
-                value={vidUrl}
-                class="input {vidUrl ? 'is-success' : 'is-primary'}"
-                readonly
-            />
+    <div class="container has-text-centered">
+        <div
+            class="file is-boxed has-name {files?.[0]
+                ? 'is-success'
+                : 'is-primary'} is-centered"
+        >
+            <label class="file-label">
+                <input
+                    class="file-input"
+                    type="file"
+                    accept="video/mp4, video/quicktime"
+                    bind:files
+                />
+                <span class="file-cta">
+                    <span class="file-label"> Upload! </span>
+                </span>
+                <span class="file-name">
+                    {files?.[0].name ?? "No file uploaded"}
+                </span>
+            </label>
         </div>
+        <progress class="progress is-primary" value={progress} max="100"
+        ></progress>
+        <input
+            type="text"
+            placeholder="No file uploaded"
+            value={vidUrl}
+            class="input {vidUrl ? 'is-success' : 'is-primary'}"
+            readonly
+        />
     </div>
 </section>
