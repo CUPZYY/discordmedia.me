a0a722e4610cbef99a5e3d9ee8c3b1e11dc00843
CUPZYY <richbanana1234@gmail.com>
2024-10-03 15:56:09 +0200

    centered text in advance settings dropdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,7 +71,9 @@
                 <p slot="title">Advanced settings</p>
                 <div slot="content" class="has-text-left">
                     <div class="columns is-mobile">
-                        <div class="column is-flex is-align-items-center">
+                        <div
+                            class="column is-flex is-justify-content-center is-align-items-center"
+                        >
                             <p class="">Custom thumbnail:</p>
                         </div>
                         <div class="column is-flex is-justify-content-center">
