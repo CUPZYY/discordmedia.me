73d793e92760e34db29c45ed98c2579da756b0f9
CUPZYY <richbanana1234@gmail.com>
2024-12-17 23:58:16 +0100

    feat: addding timestamps in database for each upload

--- src/lib/server/datebase/schema.ts
+++ src/lib/server/datebase/schema.ts
@@ -1,9 +1,13 @@
+import { sql } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const urlTable = sqliteTable("urls", {
     id: text("id").primaryKey(),
     video_url: text("video_url").notNull(),
     thumbnail_url: text("thumbnail_url"),
+    timestamp: text("timestamp")
+        .default(sql`(current_timestamp)`)
+        .notNull(),
 });
 
 export type Url = typeof urlTable.$inferSelect;
--- src/lib/server/datebase/setup.sql
+++ src/lib/server/datebase/setup.sql
@@ -1,7 +1,6 @@
-DROP TABLE urls;
-
 CREATE TABLE urls (
     id TEXT PRIMARY KEY,
     video_url TEXT NOT NULL,
-    thumbnail_url TEXT
+    thumbnail_url TEXT,
+    timestamp TEXT DEFAULT (current_timestamp) NOT NULL
 );
