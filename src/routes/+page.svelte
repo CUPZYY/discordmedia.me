0e6d11a712eaa801397636d09617f105338b0e2f
CUPZYY <richbanana1234@gmail.com>
2024-12-18 23:02:46 +0100

    Add donation text in footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -144,6 +144,15 @@
                 <strong>discordmedia.me</strong> by
                 <a href="https://github.com/CUPZYY">CUPZYY</a>
             </p>
+            <p>
+                Donations are greatly appreciated, and it helps keep our tools
+                ad-free
+                <br />
+                <a
+                    href="https://www.paypal.com/donate/?hosted_button_id=ED86J8GEUGTQL"
+                    ><strong>Donate here!</strong></a
+                >
+            </p>
         </div>
     </div>
 </section>
