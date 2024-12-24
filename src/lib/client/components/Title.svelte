81811351a97e11611ae03ab80cad61a81e4ee28e
CUPZYY <arne.habrekke@null.net>
2024-12-24 14:24:23 +0100

    fix: go back to main page

--- src/lib/client/components/Title.svelte
+++ src/lib/client/components/Title.svelte
@@ -1,5 +1,5 @@
 <div class="block has-text-centered">
-    <a href=".">
+    <a href="/">
         <h1 class="title is-1">discordmedia.me</h1>
     </a>
     <h5>Free video uploading for discord! Max size is 5GB</h5>
