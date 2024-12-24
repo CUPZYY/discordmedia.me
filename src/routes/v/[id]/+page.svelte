9a9a4180d1e37faa95bf5dee152165d5b3604eca
CUPZYY <arne.habrekke@null.net>
2024-12-24 14:29:17 +0100

    feat: add video player to video url

--- src/routes/v/[id]/+page.svelte
+++ src/routes/v/[id]/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     /** @type {import("./$types").PageData}*/
     import { page } from "$app/stores";
+    import Title from "$lib/client/components/Title.svelte";
+    import Footer from "$lib/client/components/Footer.svelte";
 
     const id = $page.params.id;
     export let data;
@@ -10,5 +12,37 @@
     <meta property="og:type" content="video.other" />
     <meta property="og:video:url" content={data.video_url} />
     <meta property="og:image" content={data.thumbnail_url} />
-    <meta http-equiv="refresh" content="0; url=/" />
 </svelte:head>
+
+<section
+    class="section is-flex is-align-items-center"
+    style="zoom: 110%; height: 100%; width: 100%"
+>
+    <div
+        class="container is-flex is-flex-direction-column"
+        style="max-width: 600px"
+    >
+        <Title></Title>
+        <div class="video-mask block">
+            <!-- svelte-ignore a11y-media-has-caption -->
+            <video
+                poster={data.thumbnail_url}
+                controls
+                controlslist="noplaybackrate nodownload"
+                disablepictureinpicture
+                disableremoteplayback
+            >
+                <source src={data.video_url} type="video/mp4" />
+            </video>
+        </div>
+        <Footer></Footer>
+    </div>
+</section>
+
+<style>
+    .video-mask {
+        overflow: hidden;
+        border-radius: 20px;
+        line-height: 0;
+    }
+</style>
