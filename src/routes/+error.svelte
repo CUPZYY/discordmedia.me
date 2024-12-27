425b43dcc262bda943180d8898f00e11bc654f98
CUPZYY <arne.habrekke@null.net>
2024-12-27 19:33:35 +0100

    feat: new error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<section>
+    <h1 class="title is-1">{$page.status}</h1>
+    <h5 class="subtitle is-5">{$page.error.message}</h5>
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
